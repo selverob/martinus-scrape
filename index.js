0465759cd174eaa07fc0f1ef73e153b9b4425fe2
Róbert Selvek <robert.selvek@gmail.com>
2014-01-28 16:16:18 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+var request = require("request")
+	, iconv = require("iconv-lite")
+	, cheerio = require("cheerio")
+	, qs = require("querystring")
+
+var get_book_URL = function(isbn, cb) {
+	var address = "http://martinus.sk?" + qs.stringify({
+			uMod: "list",
+			uTyp: "search",
+			uQ: isbn
+		});
+	request.get(address, {encoding: null}, function(err, res, body) {
+		if (err || res.statusCode != 200) {
+			cb(err, null);
+			return
+		}
+		var utfStr = iconv.decode(body, "win1250");
+		var $ = cheerio.load(utfStr);
+		var url = $("a", $(".title")).attr("href");
+		if (!url) {
+			cb(new Error("Book not found"), null);
+			return
+		}
+		cb(null, url)
+	});
+}
+
+var scrape_book_page = function(bookURL, cb) {
+	request.get(bookURL, {encoding: null}, function(err, res, body) {
+		if (err || res.statusCode != 200) {
+			cb(err, null);
+			return
+		}
+		var utfStr = iconv.decode(body, "win1250");
+		var $ = cheerio.load(utfStr);
+		var book = {
+			title: $("h1[itemprop=name]").text().trim(),
+			author: $(".subtitle > .author  strong").text(),
+			publisher: $(".publisher > a").text(),
+			pubYear: /\d\d\d\d/.exec($(".subtitle"))[0],
+			price: parseFloat($(".oldPrice > strong").text().replace(",", ".")),
+			pages: parseInt(/(\d*) strán/.exec($(".specification").text())[1])
+		}
+		cb(null, book)
+	});
+}
+
+exports.getBook = function(isbn, cb) {
+	get_book_URL(isbn.replace("-", ""), function(err, url) {
+		if (err != null) {
+			cb(err);
+			return
+		}
+		scrape_book_page(url, function(err, book) {
+			if (err != null) {
+				cb(err);
+				return
+			}
+			book.ISBN = isbn;
+			cb(null, book);
+		})
+	});
+}
+
+ exports.getBook("978805124230", function(err, b) {
+ 	if (err) {
+ 		console.log("ERROR: " + err);
+ 		return
+ 	}
+ 	console.log(b);
+ })
